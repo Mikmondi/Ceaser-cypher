d40276eb996d626658ff9fa6ad41e9d3cd0436f0
Mikmondi <mikmondi@gmail.com>
2021-06-16 07:21:42 +0300

    created an encoding class cypherEncoder

---
+++ src/main/java/models/CypherEncoder.java
@@ -0,0 +1,13 @@
+package models;
+
+public class CypherEncoder {
+    private String userInput;
+    private int shiftKey;
+    public  String encrypt(String userInput, int shiftKey){
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
