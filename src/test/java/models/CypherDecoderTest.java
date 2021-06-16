042bb6b81c565840123ba64ed730edcf185cbc56
Mikmondi <mikmondi@gmail.com>
2021-06-16 13:51:55 +0300

    test for decoding one letter with 1 key passed

--- src/test/java/models/CypherDecoderTest.java
+++ src/test/java/models/CypherDecoderTest.java
@@ -4,5 +4,10 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class CypherDecoderTest {
+    @Test
+    public void testCypherDecoder_String() throws Exception {
+        CypherDecoder testCypherDecoder = new CypherDecoder("b", 1);
+        assertEquals("a", testCypherDecoder.decryptText());
 
+    }
 }
